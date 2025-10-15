66d8aaf7ddf8f483187944e768eee41da991b8fa
diego <iseare13@gmai.com>
2025-10-15 10:09:48 -0600

    Agrega clases Settings y Ship; muestra la nave y fondo gris claro

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
-
 import pygame
+
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Clase general para gestionar los recursos y comportamientos del juego."""
@@ -8,19 +10,32 @@ class AlienInvasion:
     def __init__(self):
         """Inicializa el juego y crea recursos."""
         pygame.init()
-        
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
         
     def run_game(self):
         """Inicializa el bucle principal para el juego."""
         while True:
-            # Busca eventos de teclado y ratón
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                    
-        # Hace visible la última pantalla dibujada
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+            
+    def _check_events(self):
+        """Responde a pulsaciones de teclas y eventos de ratón."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                
+    def _update_screen(self):
+        """Actualiza las imagenes en la pantalla y cambia a la pantalla nueva."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
         pygame.display.flip()
         
         
