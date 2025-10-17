6330cbb821b44b645c3b5058d0c7b78653b8fa68
diego <iseare13@gmai.com>
2025-10-17 06:11:56 -0600

    Agrega control de movimiento lateral y modo pantalla completa; ajusta velocidad de la nave

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Inicializa la nave y configura su posición inicial."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         
         # Carga la imagen de la nave y obtiene su rect
@@ -14,6 +15,25 @@ class Ship:
         
         # Coloca inicialmente cada nave nueva en el crentro de la parte inferior de la pantalla
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        # Banderas de movimiento; empiezan con una bandera que no se mueve
+        self.moving_right = False
+        self.moving_left = False
+        
+        # Guarda un valor decimal para la posición horizontal exacta de la nave
+        self.x = float(self.rect.x)
+        
+        
+    def update(self):
+        """Actualiza la posición de la nave en función de la bandera de moviento."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+            
+        # Actualiza el objeto rect self.x
+        self.rect.x = self.x
+        
         
     def blitme(self):
         """Dibuja la nave en su ubicación actual."""
