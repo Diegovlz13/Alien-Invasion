c0d9d6f3bbc1f43b505bfaa3e8f9a73669d2beeb
diego <iseare13@gmai.com>
2025-10-22 04:56:31 -0600

    Movimiento de la flota: detección de bordes, descenso y cambio de dirección; actualiza Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
             
@@ -78,6 +79,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
                 
+    def _update_aliens(self):
+        """Comprueba si la flota está en un borde, después actualiza las posiciones."""
+        self._check_fleet_edges()
+        self.aliens.update()
+                
     def _update_screen(self):
         """Actualiza las imagenes en la pantalla y cambia a la pantalla nueva."""
         self.screen.fill(self.settings.bg_color)
@@ -106,7 +112,6 @@ class AlienInvasion:
             current_x = alien_width
             current_y += 2 * alien_height
             
-            
     def _create_alien(self, x_position, y_position):
         """Crea un alien y lo coloca en la fila."""
         new_alien = Alien(self)
@@ -114,6 +119,19 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+        
+    def _check_fleet_edges(self):
+        """Responde adecuadamente si algún alien ha llegado a un borde."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+            
+    def _change_fleet_direction(self):
+        """Baja toda la flota y cambia su dirección."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
         
         
 if __name__ == '__main__':
